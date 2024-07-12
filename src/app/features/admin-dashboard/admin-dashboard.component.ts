ee63147ae4fbb51b0ee507674bfce9cc0d83f36c
Anderson Marin <andersonarturomarin@gmail.com>
2024-07-11 20:48:32 -0500

    add guards

--- src/app/features/admin-dashboard/admin-dashboard.component.ts
+++ src/app/features/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '@shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
